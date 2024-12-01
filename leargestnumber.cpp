5d3ea8d0585b2e7527d04aadf6e426c78d9095a4
Shahriar Ahmed <c233163@ugrad.iiuc.ac.bd>
2024-12-01 18:58:23 +0600

    Add files via upload

---
+++ leargestnumber.cpp
@@ -0,0 +1,21 @@
+//largest number from a given list of integers.
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    int max = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    cout << "The largest number in the array is: " << max;
+    return 0;
+}

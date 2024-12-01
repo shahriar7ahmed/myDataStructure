ffe2508ff44387530914140b904f2ecd789baeb2
Shahriar Ahmed <c233163@ugrad.iiuc.ac.bd>
2024-12-01 19:06:37 +0600

    Add files via upload

---
+++ bubblesort.cpp
@@ -0,0 +1,31 @@
+//4.	Write a program to sort n numbers using Bubble Sort algorithm
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+    cout << "Array after sorting: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}

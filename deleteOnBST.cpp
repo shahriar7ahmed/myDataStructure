37fb2b0c1b301f1c2b1f3689fe1e42266834df96
Shahriar Ahmed <c233163@ugrad.iiuc.ac.bd>
2024-12-11 22:39:21 +0600

    Add files via upload

---
+++ deleteOnBST.cpp
@@ -0,0 +1,93 @@
+//3.	Write a program to create a Binary Search Tree of n elements and then delete an element from the tree.
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    Node *left;
+    Node *right;
+};
+Node *createNode(int data)
+{
+    Node *newNode = new Node();
+    if (!newNode)
+    {
+        cout << "Memory error\n";
+        return NULL;
+    }
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+Node *insertNode(Node *root, int data)
+{
+    if (root == NULL)
+    {
+        root = createNode(data);
+        return root;
+    }
+    if (data < root->data)
+    {
+        root->left = insertNode(root->left, data);
+    }
+    else if (data > root->data)
+    {
+        root->right = insertNode(root->right, data);
+    }
+    return root;
+}
+Node *findMin(Node *root)
+{
+    while (root->left != NULL)
+    {
+        root = root->left;
+    }
+    return root;
+}
+Node *deleteNode(Node *root, int data)
+{
+    if (root == NULL)
+    {
+        return root;
+    }
+    if (data < root->data)
+    {
+        root->left = deleteNode(root->left, data);
+    }
+    else if (data > root->data)
+    {
+        root->right = deleteNode(root->right, data);
+    }
+    else
+    {
+        if (root->left == NULL)
+        {
+            Node *temp = root->right;
+            free(root);
+            return temp;
+        }
+        else if (root->right == NULL)
+        {
+            Node *temp = root->left;
+            free(root);
+            return temp;
+        }
+        Node *temp = findMin(root->right);
+        root->data = temp->data;
+        root->right = deleteNode(root->right, temp->data);
+    }
+    return root;
+}
+int main()
+{
+    Node *root = NULL;
+    root = insertNode(root, 50);
+    root = insertNode(root, 30);
+    root = insertNode(root, 20);
+    root = insertNode(root, 40);
+    root = insertNode(root, 70);
+    root = insertNode(root, 60);
+    root = insertNode(root, 80);
+    root = deleteNode(root, 20);
+    return 0;
+}

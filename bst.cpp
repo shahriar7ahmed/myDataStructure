e03066b1da14905cfc77391bf09b66232d4782dd
Shahriar Ahmed <c233163@ugrad.iiuc.ac.bd>
2024-12-11 22:35:00 +0600

    Add files via upload

---
+++ bst.cpp
@@ -0,0 +1,86 @@
+//1.	Write a program to create a Binary Search Tree of n elements and then display the elements (preorder, inorder and postorder) of the tree.
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    Node *left;
+    Node *right;
+};
+Node *createNode(int data)
+{
+    Node *newNode = new Node();
+    if (!newNode)
+    {
+        cout << "Memory error\n";
+        return NULL;
+    }
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+Node *insertNode(Node *root, int data)
+{
+    if (root == NULL)
+    {
+        root = createNode(data);
+        return root;
+    }
+    if (data < root->data)
+    {
+        root->left = insertNode(root->left, data);
+    }
+    else if (data > root->data)
+    {
+        root->right = insertNode(root->right, data);
+    }
+    return root;
+}
+void preorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+void inorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+void postorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+int main()
+{
+    Node *root = NULL;
+    root = insertNode(root, 50);
+    insertNode(root, 30);
+    insertNode(root, 20);
+    insertNode(root, 40);
+    insertNode(root, 70);
+    insertNode(root, 60);
+    insertNode(root, 80);
+    cout << "Preorder: ";
+    preorder(root);
+    cout << "\nInorder: ";
+    inorder(root);
+    cout << "\nPostorder: ";
+    postorder(root);
+    return 0;
+}
